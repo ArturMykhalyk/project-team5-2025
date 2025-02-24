8ba18556e43a4521734911c73d435090da355591
SotnikovSS <sotnikovss90@gmail.com>
2025-02-24 19:39:13 +0100

    upd

--- src/js/order.js
+++ src/js/order.js
@@ -1,44 +1,37 @@
 (() => {
     const refs = {
         openModalBtn: document.querySelector('[data-order-open]'),
-        closeModalBtns: document.querySelectorAll('[data-order-close]'), // Все кнопки закрытия
+        closeModalBtns: document.querySelectorAll('[data-order-close]'),
         modal: document.querySelector('[data-order]'),
         form: document.querySelector('[data-order-form]'),
     };
 
-    // Открытие модального окна после отправки формы
     refs.form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Предотвращаем отправку формы (перезагрузку страницы)
-
-        // Проверка валидности формы
+        event.preventDefault();
         if (!refs.form.checkValidity()) {
-            return; // Если форма не валидна, окно не откроется
+            return;
         }
 
-        toggleModal(); // Показываем модальное окно
+        toggleModal(); 
     });
 
-    // Закрытие модального окна при нажатии на любую кнопку с атрибутом data-order-close
     refs.closeModalBtns.forEach(btn => {
         btn.addEventListener('click', () => {
-            toggleModal(); // Скрываем модальное окно
-            submitForm();  // Отправляем форму после закрытия окна
+            toggleModal();
+            submitForm();
         });
     });
 
-    // Функция для показа/скрытия модального окна
     function toggleModal() {
         refs.modal.classList.toggle('is-open');
         if (refs.modal.classList.contains('is-open')) {
-            refs.modal.style.display = 'block'; // Показываем модальное окно
+            refs.modal.style.display = 'block';
         } else {
-            refs.modal.style.display = 'none'; // Скрываем модальное окно
+            refs.modal.style.display = 'none'; 
         }
     }
 
-    // Функция для отправки формы
     function submitForm() {
-        // Отправляем форму вручную
         refs.form.submit();
     }
 })();
